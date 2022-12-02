67e95b1be88cca9fcb5331ec6864c9d49388c708
Zachary Betz <zachary.betz@hln.com>
2022-12-02 10:34:25 -0600

    uppercase vars

--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
 #/usr/bin/env bash
 
 # Make old dotfiles dir
-dotfiles_old=${HOME}/dotfiles_old
-mkdir -p ${dotfiles_old}
+DOTFILES_OLD_DIR="${HOME}/dotfiles_old"
+mkdir -p "${DOTFILES_OLD_DIR}"
 
 # Get git dotfiles
-dotfiles="$(find $(pwd)/dotfiles -type f -name ".*")"
+DOTFILES="$(find $(pwd)/dotfiles -type f -name ".*")"
 
-for f in ${dotfiles} ; do
-  dotfile=${HOME}/$(basename ${f})
+for f in ${DOTFILES} ; do
+  DOTFILE="${HOME}/$(basename ${f})"
 
   # Backup old dotfile
-  mv -f ${dotfile} ${dotfiles_old}
+  mv -f "${DOTFILE}" "${DOTFILES_OLD_DIR}/"
 
   # Symlink new dotfile
-  ln -s -v ${f} ${HOME}
+  ln -s -v "${f}" "${HOME}"
 done
 
 echo "Setup complete. Restart your Terminal"
